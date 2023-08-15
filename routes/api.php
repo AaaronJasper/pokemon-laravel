f93851459c55b4b9d66071001e5190148cbd2f96
arran <davis23232323@gmail.com>
2023-08-15 17:11:00 +0800

    pokemon

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-//註冊路由
-Route::post('register', [\App\Http\Controllers\RegisterController::class,'store']);
-//登陸路由
-Route::post('login', [\App\Http\Controllers\LoginController::class,'login']);
-Route::get("logintest",[\App\Http\Controllers\LoginController::class,'logintest']);
-Route::delete("logout",[\App\Http\Controllers\LoginController::class,'logout']);
-//用戶列表(只用index和show方法)
-Route::apiResource('users', \App\Http\Controllers\UserController::class,["only" => ["index", "show"]]);
-//測試路由
-Route::get("test",[\App\Http\Controllers\TestController::class,'index']);
-
